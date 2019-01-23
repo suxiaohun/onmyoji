7b3294c056f2df6c2c18bdb7a7b817dabcacd4ff
huxiaoyang <huxiaoyang@udesk.cn>
2019-01-23 13:55:16 +0800

    base websocket function

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    # 验证user信息,有两种形式cookie或session
+    def find_verified_user
+      verified_user = cookies[:nick_name]
+      if verified_user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+
+  root 'chat_rooms#index'
+
+  match '/auth',to: 'chat_rooms#auth',:via => [:get,:post]
+
+  get '/su',to: 'chat_rooms#su'
+
+  get '/chat_rooms/join',to: 'chat_rooms#join'
+  get '/chat_rooms/leave',to: 'chat_rooms#leave'
+
+  resources :chat_rooms
+  resources :messages
+  resources :users
+
+  #  Boolean to anchor a <tt>match</tt> pattern. Default is true. When set to
+  #  false, the pattern matches any request prefixed with the given path.
+  match '/',to: 'chat_rooms#index',anchor: false, via: :get
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
