c18c41d138987b4566b70f1b23670d42dac26215
huxiaoyang <huxiaoyang@udesk.cn>
2019-03-08 14:48:27 +0800

    dowload

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
 
-  resources :books, :only => [:index, :show]
+  get 'books/download', to: 'books#download'
+
+  resources :books
+
+
   resources :authors
   resources :categories
   match '/auth', to: 'common#auth', :via => [:get, :post]
