92d938ac97ed78c29db1f96a0f90bcfb7cc72f4c
huxiaoyang <huxiaoyang@udesk.cn>
2019-03-26 17:34:01 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,13 +37,13 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = '/websocket'
+  # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   config.action_cable.url = 'wss://www.xqianyu.com/cable'
   config.action_cable.allowed_request_origins = ['https://www.xqianyu.com' ]
