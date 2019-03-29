a71455ce26fb8e0bf30c5729221611b76ee07a84
huxiaoyang <huxiaoyang@udesk.cn>
2019-03-29 17:27:57 +0800

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -29,45 +29,64 @@ GC.respond_to?(:copy_on_write_friendly=) and
     GC.copy_on_write_friendly = true
 check_client_connection false
 
+run_once = true
 
-# before_exec { |server| ENV["BUNDLE_GEMFILE"] = "#{Rails.root}/Gemfile" }
 
-# before_fork do |server, worker|
-#   defined?(ActiveRecord::Base) and
-#       ActiveRecord::Base.connection.disconnect!
-# end
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(Rails.root, 'Gemfile')
+end
 
-#
 before_fork do |server, worker|
-  old_pid ="#{Rails.root}/tmp/pids/unicorn.pid.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  # Occasionally, it may be necessary to run non-idempotent code in the
+  # master before forking.  Keep in mind the above disconnect! example
+  # is idempotent and does not need a guard.
+  if run_once
+    # do_something_once_here ...
+    run_once = false # prevent from firing again
+  end
+
+  # The following is only recommended for memory/DB-constrained
+  # installations.  It is not needed if your system can house
+  # twice as many worker_processes as you have configured.
+  #
+  # This allows a new master process to incrementally
+  # phase out the old master process with SIGTTOU to avoid a
+  # thundering herd (especially in the "preload_app false" case)
+  # when doing a transparent upgrade.  The last worker spawned
+  # will then kill off the old master process with a SIGQUIT.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
     begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      puts "Send 'QUIT' signal to unicorn error!"
     end
   end
+
+  # Throttle the master from forking too quickly by sleeping.  Due
+  # to the implementation of standard Unix signal handlers, this
+  # helps (but does not completely) prevent identical, repeated signals
+  # from being lost when the receiving process is busy.
+  # sleep 1
 end
-#
-# before_fork do |server, worker|
-#   server.logger.info("worker=#{worker.nr} spawning in #{Dir.pwd}")
-#
-#   # graceful shutdown.
-#   old_pid_file = project_home + '/tmp/pids/unicorn.pid.oldbin'
-#   if File.exists?(old_pid_file) && server.pid != old_pid_file
-#     begin
-#       old_pid = File.read(old_pid_file).to_i
-#       server.logger.info("sending QUIT to #{old_pid}")
-#       # we're killing old unicorn master right there
-#       Process.kill("QUIT", old_pid)
-#     rescue Errno::ENOENT, Errno::ESRCH
-#       # someone else did our job for us
-#     end
-#   end
-# end
-#
-#
-# after_fork do |server, worker|
-#   defined?(ActiveRecord::Base) and
-#       ActiveRecord::Base.establish_connection
-# end
+
+after_fork do |server, worker|
+  # per-process listener ports for debugging/admin/migrations
+  # addr = "127.0.0.1:#{9293 + worker.nr}"
+  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
+
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
