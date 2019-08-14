4412eadb59e8ee4a6e7ab51436012d720bf6fb34
huxiaoyang <huxiaoyang@udesk.cn>
2019-08-14 14:55:12 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,12 @@ module Onmyoji
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    # 解决cors问题
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Origin' => 'http://linapp.udeskcat.com',
+        'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
+
     config.generators do |g|
       g.assets false
       g.helper false
