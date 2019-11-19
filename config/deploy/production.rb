37f57aa81ba37b928ead18b019c0c4bdf31465ed
huxiaoyang <huxiaoyang@udesk.cn>
2019-11-19 18:03:10 +0800

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # You can define all roles on a single server, or split them:
 
 # server "116.196.82.122", user: "crystal", roles: %w{app db web}, my_property: :my_value
-server "49.233.135.238", user: "crystal", roles: %w{app db web}, my_property: :my_value
+server "123.206.25.28", user: "crystal", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
