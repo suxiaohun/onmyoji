b228e709463e8eaf92ec9897ae07f920d62a6673
huxiaoyang <huxiaoyang@udesk.cn>
2019-12-02 17:54:00 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.11.0"
 
 set :application, "onmyoji"
 set :ruby_version, "2.6.3"
-set :repo_url, "https://github.com/suxiaohun/onmyoji.git"
+set :repo_url, "git@github.com:suxiaohun/onmyoji.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
